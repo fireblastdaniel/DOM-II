e8b8fa5c39bf0b0b8666acb7a3e52a5370736982
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-01-14 14:24:07 -0800

    4 events added, 6 to go

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+const title = document.querySelector('h1');
+title.addEventListener('click', () => {
+    title.style.color = 'red';
+})
+
+const images = document.querySelectorAll('img');
+images.forEach( element => {
+    element.addEventListener('wheel', () => {
+        element.style.transform = 'rotate(20deg)'
+    })
+})
+images.forEach( element => {
+    element.addEventListener('dblclick', () => {
+        element.style.display = 'none';
+    })
+})
+
+const welcomeTitle = document.querySelector('h2');
+document.addEventListener('keydown', event => {
+    if(event.isComposing || event.keyCode === 229){
+        return;
+    }
+    if(event.keyCode === 38){
+        welcomeTitle.style.fontSize = '500%'
+    }
+});
+
+//events used:
+//click
+//wheel
+//dblclick
+//keydown
