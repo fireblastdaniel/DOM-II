fffd397ae8732d90ce76d2069e669b516c5b2655
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-01-14 16:00:14 -0800

    all 10 events added

--- js/index.js
+++ js/index.js
@@ -26,8 +26,52 @@ document.addEventListener('keydown', event => {
     }
 });
 
+const navBar = document.querySelector('nav');
+navBar.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+
+    setTimeout( () => {
+        event.target.style.color = '';
+    }, 500);
+}, false)
+
+const allParagraph = document.querySelectorAll('p');
+allParagraph.forEach( element => {
+    window.addEventListener('resize', () => {
+        element.textContent = '';
+    })
+})
+
+let scrolls = 0;
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(element => {
+    window.addEventListener('scroll', () => {
+        element.textContent = `Cost: $${scrolls +=1}`
+    })
+})
+
+document.addEventListener('copy', e => {
+    e.clipboardData.setData('text/plain', 'This text is copyrighted data, pls do not use!!!!!1!');
+    e.preventDefault();
+})
+
+body = document.querySelector('body')
+window.addEventListener('focus', () => {
+    body.style.backgroundColor = 'hotpink'
+})
+window.addEventListener('blur', () => {
+    body.style.backgroundColor = 'blue'
+})
+
 //events used:
+
 //click
 //wheel
 //dblclick
 //keydown
+//mouseover
+//resize
+//scroll
+//copy
+//focus
+//blur
